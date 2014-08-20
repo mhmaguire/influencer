080264ebe95a9e32b26d66c0ff829ac5af354abd
Matt Maguire <mhmaguire0@gmail.com>
2014-08-19 18:39:11 -0700

    cleanup old routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
   ActiveAdmin.routes(self)
+  
   devise_for :users, skip: :registrations
   devise_for :visitors, skip: [:sessions]
-  devise_for :admins, skip: [:registrations, :sessions]
 
   root to: 'pages#index'
 
